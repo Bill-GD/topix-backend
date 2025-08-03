948336d440aed80985904b2fdeb8fd3bdf3f073c
Bill-GD <geometrydashbill@gmail.com>
2025-08-03 12:05:36 +0700

    Logs host & port when started

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3000;
+  console.log(`[Server]\tLocal: http://localhost:${port}/`);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 
 bootstrap();
