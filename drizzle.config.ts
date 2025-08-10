ca71f861e1b8061856c0f15939cc046c39816cbe
Bill-GD <geometrydashbill@gmail.com>
2025-08-10 15:11:56 +0700

    setting up project
    - packages
    - import alias
    - env template
    - handlebars template for mail
    - open api docs
    - switched to drizzle & config
    - index files for importing

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: [
+    './src/database/schemas/**/*.ts',
+    '!./src/database/schemas/index.ts',
+  ],
+  dialect: 'mysql',
+  dbCredentials: {
+    host: String(process.env.DATABASE_HOST),
+    port: Number(process.env.DATABASE_PORT),
+    user: String(process.env.DATABASE_USER),
+    password: String(process.env.DATABASE_PASSWORD),
+    database: String(process.env.DATABASE_NAME),
+  },
+  verbose: process.env.ENVIRONMENT === 'development',
+});
