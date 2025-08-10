86a346e056bc7e74fdcf4ed3f2f4e04c039be5bb
Bill-GD <geometrydashbill@gmail.com>
2025-08-10 15:11:56 +0700

    setting up project
    - packages
    - import alias
    - env template
    - handlebars template for mail
    - open api docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   console.log(`[Server]\tLocal: http://localhost:${port}/`);
 
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('topix API')
+    .setDescription(
+      'The API server for ' +
+        '<b><a target="_blank" href="https://github.com/Bill-GD/topix">topix</a></b>',
+    )
+    .setVersion(process.env.npm_package_version ?? '0.0.1')
+    .build();
+  SwaggerModule.setup('api-docs', app, () =>
+    SwaggerModule.createDocument(app, swaggerConfig),
+  );
+
   await app.listen(port);
 }
 
